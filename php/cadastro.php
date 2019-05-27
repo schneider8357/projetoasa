f203665df33a18b8ffe41993758a8e8b9f651fea
schneider8357 <schneider8357@hotmail.com>
2019-05-27 14:05:13 -0700

    Cadastro de usuario (falta testar)

--- php/cadastro.php
+++ php/cadastro.php
@@ -6,12 +6,12 @@
 function insere($valores) {
 // Formato: (ip-do-servidor,usuario,senha,banco)
 echo $valores;
-/*
+
 $con = new mysqli("127.0.0.1","ftpdbuser","ftpdbuserpwd","ASA");
 if ($con->connect_error)
   return"Não foi possível conectar com o banco: " . $conn->connect_error;
 
-$sql=addslashes("INSERT INTO ftpusers VALUES ()");
+$sql=addslashes("INSERT INTO ftpusers VALUES $valores");
 
 if ($con->query($sql) === TRUE)
   echo "Query OK";
@@ -19,7 +19,7 @@ else
   return "Erro: " . $conn->error;
 
 $con->close();
-*/
+
 
 }
 ?>
--- php/cadastro_usuario.php
+++ php/cadastro_usuario.php
@@ -1,20 +1,16 @@
- 
+<!DOCTYPE html>
+<html>
+<body>
 <?php
-define('ROOT_PATH', '/root/projetoasa');
-include 'php/cadastro.php';
-$valores = "'$_POST['nome']', '$_POST['login']', '$_POST['senha']', $_POST['uid'] , $_POST['gid'], '$_POST['ativo']', '$_POST['dir']', '$_POST['shell']', '$_POST['email']'"
+define('ROOT_PATH', '/var/www/html/projetoasa');
+include ROOT_PATH . '/php/cadastro.php';
+
 // Formato: (ip-do-servidor,usuario,senha,banco)
-$con = new mysqli("127.0.0.1","ftpdbuser","ftpdbuserpwd","ASA");
-if ($con->connect_error)
-  die("Não foi possível conectar com o banco: " . $conn->connect_error);
-
-$sql=addslashes("INSERT INTO ftpusers VALUES ()");
-
-if ($con->query($sql) === TRUE)
-  echo "Query OK";
-else
-  die("Erro: " . $conn->error);
-
-$con->close();
-
+$valores = "testando";
+$valores = "('$_POST[nome]', '$_POST[login]', '$_POST[senha]', $_POST[uid] , $_POST[gid], '$_POST[ativo]', '$_POST[dir]', '$_POST[shell]', '$_POST[email]')";
+echo $valores;
+insere($valores);
 ?>
+
+</body>
+</html>
