c7ac5d6be0a6708a40654ebf0374fc56da4236b2
schneider8357 <schneider8357@hotmail.com>
2019-06-10 20:45:56 -0300

    Arrumar Merge

--- php/altera_usuario_form.php
+++ php/altera_usuario_form.php
@@ -4,15 +4,9 @@
 <h1>Alterar dados de usuário</h1>
 <form action="altera_usuario.php" method="post">
 <?php
-<<<<<<< HEAD
 include_once 'mod_tabela_usuarios.php';
 
 $row = lista_um($_GET['email']);
-=======
-include 'acesso_banco.php';
-
-$row = lista_um($_GET[email]);
->>>>>>> 5114b759109b0a903c9412def6dddda94bcab2dc
 echo 'Nome: <input type="text" name="nome" value="' . $row['nome'] . '" /><br><br>';
 echo 'Login: <input type="text" name="login" value="' . $row['login'] . '" /><br><br>';
 echo 'Senha: <input type="password" name="senha" value="' . $row['senha'] . '" /><br><br>';
